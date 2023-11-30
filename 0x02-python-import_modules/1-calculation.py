7147dfc18b6d2e202f5c31a184c30b205ee20bd6
folujam <olusway@gmail.com>
2023-11-30 23:16:38 +0100

    modif print

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,12 +2,8 @@
 from calculation_1 import add, sub, mul, div
 a = 10
 b = 5
-resultA = add(a, b)
-resultS = sub(a, b)
-resultM = mul(a, b)
-resultD = div(a, b)
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, resultA))
-    print("{} - {} = {}".format(a, b, resultS))
-    print("{} * {} = {}".format(a, b, resultM))
-    print("{} / {} = {}".format(a, b, resultD))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
