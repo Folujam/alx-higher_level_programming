7b863540c755f10caab2e9602da91dea2568e1d0
folujam <olusway@gmail.com>
2023-11-30 23:22:40 +0100

    repositioned exe block to top

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-from calculation_1 import add, sub, mul, div
-a = 10
-b = 5
 if __name__ == "__main__":
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    from calculation_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    resultA = add(a, b)
+    resultS = sub(a, b)
+    resultM = mul(a, b)
+    resultD = div(a, b)
+    print("{} + {} = {}".format(a, b, resultA))
+    print("{} - {} = {}".format(a, b, resultS))
+    print("{} * {} = {}".format(a, b, resultM))
+    print("{} / {} = {}".format(a, b, resultD))
