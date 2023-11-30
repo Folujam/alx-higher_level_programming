7dd64820da86e1c03821f0bcb247d80c7d580fe8
folujam <olusway@gmail.com>
2023-11-30 23:42:37 +0100

    restructured condi

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,13 @@
 import sys
 if __name__ == "__main__":
     numargs = len(sys.argv) - 1
-    if numargs > 1:
-        print(f"{numargs} arguments:")
-    elif numargs == 1:
-        print(f"{numargs} argument:")
-    elif numargs == 0:
+    if numargs == 0:
         print(f"{numargs} arguments.")
-    for i in range(1, numargs + 1):
+        return
+    else:
+        if numargs == 1:
+            print(f"{numargs} argument:")
+        elif numargs > 1:
+            print(f"{numargs} arguments:")
+        for i in range(1, numargs + 1):
         print(f"{i}: {sys.argv[i]}")
