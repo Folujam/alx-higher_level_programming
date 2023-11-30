b304201e34e9c015cdb1dab8c333d9204371b268
folujam <olusway@gmail.com>
2023-11-30 21:12:24 +0100

    1.run & removed blank line in 0.6

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import calculation_1:
+    a = 10
+    b = 5
+    resultA = calculation_1.add(a, b)
+    resultS = calculation_1.sub(a, b)
+    resultM = calculation_1.mul(a, b)
+    resultD = calculation_1.div(a, b)
+    if __name__ == "__main__":
+        print("{} + {} = {}".format(a, b, resultA))
+        print("{} - {} = {}".format(a, b, resultS))
+        print("{} * {} = {}".format(a, b, resultM))
+        print("{} / {} = {}".format(a, b, resultD))
