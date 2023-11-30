3784c7cb1f0c93a75aad9b6fe38a161f34d5916f
folujam <olusway@gmail.com>
2023-12-01 00:24:25 +0100

    removed return

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
     numargs = len(sys.argv) - 1
     if numargs == 0:
         print(f"{numargs} arguments.")
-        return
     else:
         if numargs == 1:
             print(f"{numargs} argument:")
