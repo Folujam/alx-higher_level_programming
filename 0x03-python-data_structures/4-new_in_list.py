fc3947d803f46cf6a3d5dea580af69fee881a9fb
folujam <olusway@gmail.com>
2023-12-03 23:03:12 +0100

    clrd whtspc

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     a = my_list[:]
     if idx < 0 or idx >= len(my_list):
-        return(a)
+        return (a)
     else:
         a[idx] = element
-        return(a)
+        return (a)
