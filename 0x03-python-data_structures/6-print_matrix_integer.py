e7aaf8f332dde8f6dd0d7ca2a4cb616b35fcc042
folujam <olusway@gmail.com>
2023-12-04 15:48:04 +0100

    new code structure && commented another code 4 sam ething

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,11 @@ def print_matrix_integer(matrix=[[]]):
             row_as_strings.append("{:d}".format(elem))
         print(" ".join(row_as_strings))
 
+
+#
+'''
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for elem in row:
             print(" ".join("{:d}".format(elem) for elem in row))
+'''
