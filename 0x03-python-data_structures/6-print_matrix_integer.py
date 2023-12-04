f590977f480d7613281e7859f97cf5222bcff404
folujam <olusway@gmail.com>
2023-12-04 15:41:38 +0100

    did two methods to test if it'll work

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        row_as_strings = []
         for elem in row:
-            print("{:d}".format(elem), end=" ")
-        print()
+            row_as_strings.append("{:d}".format(elem))
+        print(" ".join(row_as_strings))
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for elem in row:
+            print(" ".join("{:d}".format(elem) for elem in row))
