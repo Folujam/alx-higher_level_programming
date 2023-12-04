abe2bf5e04ef760092a54326094a7320a3ca2e94
folujam <olusway@gmail.com>
2023-12-04 15:20:15 +0100

    created new list and appended t or f

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if len(my_list) is 0:
+    if len(my_list) == 0:
         return
     else:
+        nw_lst = []
         for i in my_list:
-            if i % 2 is 0:
-                return True
+            if i % 2 == 0:
+                nw_lst.append(True)
             else:
-                return False
+                nw_lst.append(False)
+    return nw_lst
