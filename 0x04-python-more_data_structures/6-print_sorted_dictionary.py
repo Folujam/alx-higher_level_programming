eab9bf4ba74852a96632e5b39dc1af55ff5755aa
folujam <olusway@gmail.com>
2023-12-06 23:58:01 +0100

    checked idict first to work for 7

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    keys_lst = list(a_dictionary.keys())
-    sortd_list = sorted(keys_lst)
-    for key in sortd_list:
-        print("{}: {}".format(key, a_dictionary[key]))
+    if a_dictionary is not None:
+        keys_lst = list(a_dictionary.keys())
+        sortd_list = sorted(keys_lst)
+        for key in sortd_list:
+            print("{}: {}".format(key, a_dictionary[key]))
+    else:
+        print("Error: The dictionary is None")
