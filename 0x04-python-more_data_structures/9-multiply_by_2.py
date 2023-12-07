9ab81f8a708c0ea42bcea31ed497a721db113b2c
folujam <olusway@gmail.com>
2023-12-07 15:26:10 +0100

    unpacked values too

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dic = {}
-    for key, value in a_dictionary:
+    for key, value in a_dictionary.item():
         new_dic[key] = value * 2
     return (new_dic)
