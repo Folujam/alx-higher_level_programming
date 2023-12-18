54bd6a51d1e318a1d65d9b16bdb9198a4d3b6575
folujam <olusway@gmail.com>
2023-12-18 22:05:24 +0100

    tsk0 f & tsk2 runs

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    countr = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            countr += 1
+        except (IndexError, TypeError):
+            continue
+    print()
+    return (countr)
