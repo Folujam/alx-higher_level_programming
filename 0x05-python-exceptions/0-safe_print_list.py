7b6afa0122cc97742d46f9646caf8aab8b710ad7
folujam <olusway@gmail.com>
2023-12-18 20:20:17 +0100

    init and tsk0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    countr = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            countr += 1
+        except IndexError:
+            break
+    return (countr)
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+this is the Python EXCEPTIONS directory
