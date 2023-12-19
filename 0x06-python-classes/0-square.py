ad8eb0585409601ba29f2d4ef0d4eaa61675dc92
folujam <olusway@gmail.com>
2023-12-19 22:57:51 +0100

    corrected docu style

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-""" this class defines a square with no instantiation"""
+""" 
+this class defines a square with no instantiation
+"""
 
 
 class square:
-    """this is the class that defines a square"""
+    """
+    this is the class that defines a square
+    """
 
 
     pass
