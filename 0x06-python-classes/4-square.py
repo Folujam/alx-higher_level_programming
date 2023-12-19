bd62d574f1e77777f50908e3e006263513ae16b4
folujam <olusway@gmail.com>
2023-12-20 00:03:15 +0100

    mmk

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,7 +17,7 @@ class Square:
     private instance attribute: size
     """
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size
 
         @property
         def size(self):
