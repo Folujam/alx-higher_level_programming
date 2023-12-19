1d24c051d798d6280f3df31174177d0b35ee7404
folujam <olusway@gmail.com>
2023-12-19 23:34:58 +0100

    2 corrected docu & 3 runs

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-a class Square that defines a square by: (based on 1-square.py)
+a class Square that defines a square by: (based on 2-square.py)
 
 Private instance attribute: size
 Instantiation with optional size: def __init__(self, size=0):
-size must be an integer, otherwise raise a TypeError exception
-if size is less than 0, raise a ValueError exception
-You are not allowed to import any module
+size must be an integer, otherwise raise a TypeError exception 
+if size is less than 0, raise a ValueError exception 
+Public instance method: def area(self):returns the current square area
 """
 
 
@@ -20,3 +20,5 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+    def area(self):
+        return self.__size ** 2
