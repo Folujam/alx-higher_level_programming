ed1b2f01a77586d0b83261d826b474cc5fa221ce
folujam <olusway@gmail.com>
2024-01-05 23:37:38 +0100

    docs added with nothin to say

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+docu goes here
+"""
 class LockedClass:
+    """
+    docu
+    """
     def __setattr__(self, name, value):
         if name != "first_name":
-            raise AttributeError("You can only create 'first_name' attribute")
+            raise AttributeError("'LockedClass' object has no attribute 'last_name'")
         self.__dict__[name] = value
