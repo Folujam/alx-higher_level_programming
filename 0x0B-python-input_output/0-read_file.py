753ebae021e9fbb4049880725b51e98662e78346
folujam <olusway@gmail.com>
2024-01-09 22:27:30 +0100

    no test

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this reads a file
+"""
+
+
+def read_file(filename=""):
+    """
+    this function opens a file in read mode and prints it
+    """
+    with open(filename, encoding="utf-8") as afile:
+        re = afile.read()
+        print(re)
