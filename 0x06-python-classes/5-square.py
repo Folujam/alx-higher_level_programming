dc924c2958ef7618803026d1c229f401cffddde4
folujam <olusway@gmail.com>
2024-01-09 10:26:52 +0100

    4&5 py.classes && testdev init & t0

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -22,26 +22,26 @@ class Square:
     def __init__(self, size=0):
         self.size = size
 
-        @property
-        def size(self):
-            return self.__size
+    @property
+    def size(self):
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-        def area(self):
-            return self.__size ** 2
+    def area(self):
+        return self.__size ** 2
 
-        def my_print(self):
-            if self.__size > 0:
-                for a in range(self.__size):
-                    for b in range(self.__size):
-                        print("#", end="")
-                    print()
-            else:
+    def my_print(self):
+        if self.__size > 0:
+            for a in range(self.__size):
+                for b in range(self.__size):
+                    print("#", end="")
                 print()
+        else:
+            print()
