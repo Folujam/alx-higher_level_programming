4fe004913a22a4b68eb02e6ed587d0be9f1d083b
folujam <olusway@gmail.com>
2024-01-09 22:47:42 +0100

    written txt

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+write to a file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    function writes a string to a txtfile and returns num of chars
+    """
+    with open(filename, mode= "w", encoding="utf-8") as afile:
+        return afile.write(text)
