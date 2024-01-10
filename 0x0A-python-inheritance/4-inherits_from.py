542681096ffb3c2d85e302ad48de4751a97839aa
folujam <olusway@gmail.com>
2024-01-10 12:59:23 +0100

    done

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+only sub class of 
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    In this code, isinstance(obj, a_class) checks if obj is an
+    instance of a_class or a subclass of a_class, and type(obj)
+    is not a_class checks if obj is not an instance of a_class itself
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
