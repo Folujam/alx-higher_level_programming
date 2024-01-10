01471c0fe029c0b9fa3e37449fcf0b83c2287300
folujam <olusway@gmail.com>
2024-01-10 11:42:49 +0100

    added class

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -4,6 +4,16 @@ student to JSON with filter
 """
 
 
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
+        define your Student class and initialize your
+        instance attributes in the __init__ method
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
 def to_json(self, attrs=None):
     """
     In this code, isinstance(attrs, list) checks if attrs is a list,
