9d8f1ddb4aaa398981a56d4b315c94fb594b3929
folujam <olusway@gmail.com>
2024-01-10 13:11:29 +0100

    build area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+geometry module
+"""
+
+
+class BaseGeometry:
+    """
+    public instances method and exception
+    """
+    def area(self):
+        """
+        In this code, raise Exception("area() is not implemented")
+        raises an Exception with the message "area() is not implemented"
+        """
+        raise Exception("area() is not implemented")
