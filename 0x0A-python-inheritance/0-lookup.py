3922e5cd23185b87454ed7f9c2fd993446847e32
folujam <olusway@gmail.com>
2024-01-10 12:20:09 +0100

    init && 0t

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+look up
+dir() function to get a list of all the attributes
+and methods of an object.
+"""
+
+
+def lookup(obj):
+    """
+    In this code, dir(obj) returns a list of
+    all the attributes and methods of the object obj.
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+this is the inheritance directory
