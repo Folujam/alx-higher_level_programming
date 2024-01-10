40525c3c3c7b4ce283156a30b151cafed262bedb
folujam <olusway@gmail.com>
2024-01-10 23:43:55 +0100

    base added

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,6 +4,23 @@ rectangle inherits from basegeometry
 """
 
 
+class BaseGeometry:
+    """
+    public instances method and exception
+    """
+    def area(self):
+        """
+        In this code, raise Exception("area() is not implemented")
+        raises an Exception with the message "area() is not implemented"
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validation(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
 class Rectangle(BaseGeometry):
     """
     rectangle de inherit BG
