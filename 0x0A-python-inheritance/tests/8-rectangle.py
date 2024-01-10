a4a202c8b5dda8d231ef0fd8e374bd19fc972c98
folujam <olusway@gmail.com>
2024-01-10 13:34:56 +0100

    prob

--- 0x0A-python-inheritance/tests/8-rectangle.py
+++ 0x0A-python-inheritance/tests/8-rectangle.py
@@ -9,6 +9,13 @@ class Rectangle(BaseGeometry):
     rectangle de inherit BG
     """
     def __init__(self, width, height):
+        """
+        In this code, self.integer_validator("width", width) 
+        and self.integer_validator("height", height) call the
+        integer_validator method to validate width and height,
+        and self.__width = width and self.__height = height assign
+        these values to private instance variables
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
