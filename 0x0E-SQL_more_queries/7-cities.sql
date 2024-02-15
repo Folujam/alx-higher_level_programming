71a7ad419651af26d3a21ff7f7aa8abc4aeee1c3
folujam <olusway@gmail.com>
2024-02-15 22:40:28 +0100

    test 1

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,4 +2,5 @@
 -- because Batch 3 is th best!
 --  creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE If NOT EXISTS hbtn_0d_usa.states(`id` INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, `name` VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(`id` INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+    `name` VARCHAR(256) NOT NULL);
