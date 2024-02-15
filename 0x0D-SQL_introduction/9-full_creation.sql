b1846be781a6d3689283cf2fc6655fff647f6603
folujam <olusway@gmail.com>
2024-02-15 15:53:10 +0100

    cc:ful

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,5 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is th best!
+--creates second table and inserts new values
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
