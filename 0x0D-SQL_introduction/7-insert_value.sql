d79ac4888a4374458759d3b4aab3ef03e7ce9769
folujam <olusway@gmail.com>
2024-02-15 15:29:57 +0100

    nwrcopy

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is th best!
 -- inserts a new row
-INSERT INTO first_table VALUES(89, "Holberton School");
+INSERT INTO first_table VALUES(id=89, name="Best School");
