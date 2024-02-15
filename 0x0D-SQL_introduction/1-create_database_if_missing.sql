2728ac961858e685c71391ba654c9ad5a61385d3
folujam <olusway@gmail.com>
2024-02-15 23:53:18 +0100

    backticks

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is th best!
 --creates database that doesnt fail
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
