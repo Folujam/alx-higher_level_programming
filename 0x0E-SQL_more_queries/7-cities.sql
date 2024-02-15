e245761feb09e63312140a70655b488735fef586
folujam <olusway@gmail.com>
2024-02-15 22:35:07 +0100

    test

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,5 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is th best!
+--  creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE If NOT EXISTS hbtn_0d_usa.states(`id` INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, `name` VARCHAR(256) NOT NULL);
