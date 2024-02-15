33629c170241d4dc76411c86454703d1505fd9ac
folujam <olusway@gmail.com>
2024-02-16 00:31:41 +0100

    9goes 101x

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,4 +2,4 @@
 -- because Batch 3 is th best!
 --creates second table and inserts new values
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table VALUES(1,"John",10), (2,"Alex",3), (3,"Bob",14), (4,"George",8);
+INSERT INTO second_table (id, name, score) VALUES(1,"John",10), (2,"Alex",3), (3,"Bob",14), (4,"George",8);
