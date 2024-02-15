fdcc2079b9642516e3d2014bdb693525d3ffed50
folujam <olusway@gmail.com>
2024-02-15 21:35:16 +0100

    TEST

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+-- creates the MySQL server user 
+GRANT ALL PRIVILEGES ON * . *to 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd'
