46d709fe5f8be081d1984d61ab0a908bce50bcfd
folujam <olusway@gmail.com>
2024-02-15 21:47:52 +0100

    lets test

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
 -- creates the MySQL server user 
-GRANT ALL PRIVILEGES ON * . *to 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd'
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
