7bae4188b8417dbd8b7acaddb068043df40dae2e
folujam <olusway@gmail.com>
2024-02-15 14:56:45 +0100

    TEST

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is th best!
 --creates database that doesnt fail
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+CREATE DATABASE hbtn_0c_0 IF NOT EXISTS;
