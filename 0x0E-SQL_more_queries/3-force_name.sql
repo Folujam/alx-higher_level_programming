1029e58d00c3c47d490090ae3eeef51496f2819e
folujam <olusway@gmail.com>
2024-02-15 21:52:41 +0100

    int

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is th best!
+-- that creates the table force_name on your MySQL server
+CREATE TABLE IF NOT EXISTS `force_name` (
+	`id` INT,
+	`name` VARCHAR(256) NOT NULL);
