a6c5585212b4f42237b2096f1e130c0e2170fe54
folujam <olusway@gmail.com>
2024-03-12 02:44:10 +0100

    2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* prints argument availability status*/
+if (process.argv.lenght === 2){
+    console.log('No argument');
+}else if (process.argv.lenght === 3){
+    console.log('Argument found');
+}else{
+    console.log('Arguments found');
+}
