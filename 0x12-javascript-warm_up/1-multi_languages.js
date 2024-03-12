059865a33422f71e787245e4cb968ebfb992fa2a
folujam <olusway@gmail.com>
2024-03-12 02:14:12 +0100

    now single quote

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 /*a script that print 3 lines of txt*/
-let myVar1 = "C is fun";
-let myVar2 = "Python is cool";
-let myVar3 = "JavaScript is amazing";
+let myVar1 = 'C is fun';
+let myVar2 = 'Python is cool';
+let myVar3 = 'JavaScript is amazing';
 console.log(myVar1);
 console.log(myVar2);
 console.log(myVar3);
