b6189d3096030b52ab7ebe0a974f3e1db91c315e
folujam <olusway@gmail.com>
2024-03-19 10:48:17 +0100

    test cleared

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-/* prints two arguments in format of "is" btw args eg. c[a1] is[constant] cool[a2]*/
+/* prints two arguments in format of "is" btw args eg. c[a1] is[constant] cool[a2] */
 
 const a1 = process.argv[2];
 const a2 = process.argv[3];
 
-console.log(a1 +" " + "is" + " " + a2);
+console.log(a1 + ' ' + 'is' + ' ' + a2);
