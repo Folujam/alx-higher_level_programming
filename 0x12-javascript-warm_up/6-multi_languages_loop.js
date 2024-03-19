ee6dd4eddb8b3bee57e243c4378edb2dea47abd3
folujam <olusway@gmail.com>
2024-03-19 15:24:36 +0100

    gtg6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* script prints 3 lines */
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arrlen = array.length;
+let i = 0;
+while (i < arrlen) {
+  console.log(array[i]);
+  i++;
+}
