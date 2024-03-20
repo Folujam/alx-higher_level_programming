4ec69d6345644713229e9b59604b687e56feddb3
folujam <olusway@gmail.com>
2024-03-20 13:58:23 +0100

    redem

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,11 +2,11 @@
 /* script computes and prints a factorial */
 
 const n = Number(process.argv[2]);
-function factorial(n) {
-    if (n <= 1 || isNaN(n)) {
-        return (1);
-    } else {
-        return (n * (n - 1));
-    }
+function factorial (n) {
+  if (n <= 1 || isNaN(n)) {
+    return (1);
+  } else {
+    return (n * (n - 1));
+  }
 }
 console.log(factorial(n));
