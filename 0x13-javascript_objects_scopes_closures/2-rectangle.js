28a4afa713ce0142206ded53789ce776a2d6f48a
folujam <olusway@gmail.com>
2024-03-20 22:45:06 +0100

    hy

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* class that defines a Rectangle */
+
+export default class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
