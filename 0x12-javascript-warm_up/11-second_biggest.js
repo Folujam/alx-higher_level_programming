7582964361c2bc75719d40ea9431bf0f339f7c06
folujam <olusway@gmail.com>
2024-03-20 14:43:29 +0100

    test

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* script that searches the second biggest integer in the list of arguments */
+
+const list = process.argv.slice(2).map(Number);
+let bignum = 0;
+if (list.length < 2) {
+  console.log('0');
+} else {
+  list.sort((a, b) => a - b);
+  bignum = list[list.length - 2];
+}
+console.log(bignum);
