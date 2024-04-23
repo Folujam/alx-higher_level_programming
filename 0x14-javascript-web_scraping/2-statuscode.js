c40d9a9d6311e9eccc3c5870e615dbc0782186c7
folujam <olusway@gmail.com>
2024-04-23 23:34:39 +0100

    go go

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Script that display the status code of a GET request
+
+const args = process.argv;
+let request = require('request');
+request(args[2], (error, response, body) => {
+  if (error) {
+    console.log('error:', error);
+  } else console.log('code:', response && response.statusCode);
+});
