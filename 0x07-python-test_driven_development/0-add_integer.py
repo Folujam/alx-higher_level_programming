ff191c98f6da05c7cdd66ffdcc15e38071837733
folujam <olusway@gmail.com>
2024-05-10 11:42:22 +0100

    correction done to trace back, pycodestyle

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
+import doctest
+doctest.testfile("0-add_integer.txt")
+
 """
 this is to add two integers and nothing else
 """
 
+
 def add_integer(a, b=98):
     """ this function checks & ensures a&b are integers,
     if they are floats - converts them to int by rounding them down.
-    returns the sum. 
+    returns the sum.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
-    
+
     return (a + b)
