f39ce93da83fdf0b6275a6aa9b3dc155f8499e90
folujam <olusway@gmail.com>
2024-05-13 23:52:14 +0100

    test

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+write a class rectangle
+def width, height, return area, return perimeter
+"""
+
+
+class Rectangle:
+    """
+    a defined class
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width is 0 or self.height is 0:
+            return 0
+        p = self.height + self.width
+        return 2 * p
+
+    def print(self):
+        if self.width is 0 or self.height is 0:
+            return ''
+        for _ in range(self.width):
+            print('#'.repeat(self.height))
+
+    def str(self):
+        if self.width is 0 or self.height is 0:
+            return ''
+        for _ in range(self.width):
+            print('#'.repeat(self.height))
