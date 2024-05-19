42cf579eaf8b37473c57f4ef9837e0c2c9674019
folujam <olusway@gmail.com>
2024-05-19 12:09:28 +0100

    lests

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,10 +9,4 @@ class MyList(list):
     class of list
     """
     def print_sorted(self):
-        """
-        In this code, sorted(self) returns a new list
-        that contains all items from self (the MyList instance)
-        in ascending order. The print() function is used to print
-        this sorted list.
-        """
         print(sorted(self))
