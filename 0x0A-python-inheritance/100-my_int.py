11a717877045bb6873e697bc0171f218d37b7d91
folujam <olusway@gmail.com>
2024-05-20 23:43:30 +0100

    rem t12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""
+creates a myint inherited from Int and it rebels
+"""
+
+
 class MyInt(int):
-    def __init__(self, value):
-        self.num = value
+    """
+    this Class behaves like an int but Rebels
+    """
 
-    def __eq__(self, other):
-        return self.num != other
+    def __eq__(self, other_num):
+        return super().__ne__(other_num)
 
-    def __ne__(self, other):
-        return self.num == other
+    def __ne__(self, other_num):
+        return super().__eq__(other_num)
