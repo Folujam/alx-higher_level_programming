df1b650a80f86ccdac6c6e68849f3972492b2039
folujam <olusway@gmail.com>
2024-05-20 23:25:44 +0100

    test t11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+a square module that inherites basegeometry
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """this class(sqr) inherites from rectangle and uses the area
+    and integer_validator method"""
+
+    def __init__(self, size):
+        """inits positive size && uses int_vali to ensure its int"""
+        self.__size = size
+        super().integer_validator('size', size)
+
+    def area(self):
+        """Calculate the area of a Square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        return (f"[Square] {self.__size}/{self.__size}")
