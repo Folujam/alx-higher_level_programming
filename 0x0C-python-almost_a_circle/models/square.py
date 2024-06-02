c46e12b09119b9a5f9ffd92e1a16a16f9eb0cbcc
folujam <olusway@gmail.com>
2024-06-02 10:23:09 +0100

    test2

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,11 +9,14 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """
-    this is the definition of Sqaure which inherits from 
+    this is the definition of Sqaure which inherits from
     Rectangle(Base)
     """
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, x, y, size, size)
+        """
+        initializing parameters(attributes) listed above
+        """
+        super().__init__(id, x, y, width=size, height=size)
 
     def __str__(self):
         """returns str info for square"""
