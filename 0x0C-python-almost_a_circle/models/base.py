f2f6383349a1d76476a6acd5fc5f58e212810099
folujam <olusway@gmail.com>
2024-10-06 14:23:35 +0100

    to_json_strin implemented

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 base class
 """
+import json
 
 
 class Base:
@@ -19,3 +20,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """method returns the JSON string rep of arg"""
+        if list_dictionaries is not None or len(list_dictionaries) != 0:
+            return json.dumps(list_dictionaries)
+        return list_dictionaries
