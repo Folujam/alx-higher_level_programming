e5dbfc6fdf9abc9fd68b0d9b583243ef31a792d7
folujam <olusway@gmail.com>
2024-10-06 10:16:10 +0100

    to_dictionary implemented

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -75,3 +75,12 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """returns dictionary representation of class"""
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
